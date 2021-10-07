cd66aa5cfa72857a440b1f0773de4b681f97e695
Toon Proost <proost.toon@gmail.com>
2021-10-07 08:57:08 +0200

    updated licence notice

--- sellotron3000.py
+++ sellotron3000.py
@@ -1,12 +1,26 @@
-##################
-# sellotron3000 #
-##################
+#    Sellotron3000 a water distribution vending machine
+#    Copyright (C) 2021  Toon Proost
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-#
-#
-#
-# V 0.0.1 (alpha)
-#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+
+
+#   V 0.0.1 (alpha)
+
 
 
 
